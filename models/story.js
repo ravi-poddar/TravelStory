040b6750ae40b81f228fe599bd5ddd5b6994f1c7
Ravi Kumar <57840416+ravi-poddar@users.noreply.github.com>
2020-07-26 10:48:57 +0530

    schema defined (user, story, comment)

---
+++ models/story.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+//Story Schema - name,image,description
+var storySchema = new mongoose.Schema({
+    name : String,
+    image : String,
+    description : String,
+    author : {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments :[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref  : "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Story",storySchema);
